f3bad4f745889271407fa0fa45c7f1e3aaf69466
PPSHAH12 <81774561+PPSHAH12@users.noreply.github.com>
2022-04-13 19:39:52 +0530

    table fixed

--- admin-panel/src/App.js
+++ admin-panel/src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Dashboard from "./components/Dashboard.js";
+
 import Contacts from "./components/Contacts.js";
 import FoodItems from "./components/FoodItems.js";
 import Foodoutlet from "./components/Foodoutlet.js";
@@ -18,15 +18,14 @@ function App() {
       <Router>
       <SideNav/>
         <Routes >
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/food-items" element={<FoodItems />} />
-          <Route path="/food-outlets" element={<Foodoutlet />} />
-          <Route path="/mess-menu" element={<MessMenu />} />
-          <Route path="/role" element={<Role />} />
-          <Route path="/user" element={<User />} />
-          <Route path="/bus-timings" element={<BusTimings />} />
-          <Route path="/ferry-timings" element={<FerryTimings />} />
+        <Route path="/contacts" element={<></>} />
+          <Route path="/food-items" element={<></>} />
+          <Route path="/food-outlets" element={<></>} />
+          <Route path="/mess-menu" element={<></>} />
+          <Route path="/role" element={<></>} />
+          
+          <Route path="/bus-timings" element={<></>} />
+          <Route path="/ferry-timings" element={<></>} />
         </Routes>  
       </Router>
     </>
